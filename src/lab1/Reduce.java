a2df0b7e07a8ed29c104056388c84320f2fe2604
Ryan-Xue2 <69762123+Ryan-Xue2@users.noreply.github.com>
2025-09-08 10:57:02 -0400

    added reduce

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,17 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int steps = 0;
+        while (i > 0) {
+            if (i % 2 == 0) {
+                i /= 2;
+            } else {
+                i -= 1;
+            }
+            steps++;
+        }
+        System.out.println(steps);
+    }
+}
