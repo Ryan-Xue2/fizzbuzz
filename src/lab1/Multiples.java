ac1d3474d397a7f8d1de63ce3a4e03e9b2a5c70f
Ryan-Xue2 <69762123+Ryan-Xue2@users.noreply.github.com>
2025-09-08 10:25:29 -0400

    extracted method from multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,12 +2,16 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int ans = 0;
-        for (int i = 1, i < 1000, i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                ans++;
-            }  
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+    }
+    public static void multiples(int n, int a, int b) {
+        int res = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                res++;
+            } 
         }
-        System.out.println(ans);
+        return res;
     }
 }
